c73f08e7450bf3822bc731fa21e12b3fcab0ee6f
roman-jordan <rjordan1352@gmail.com>
2019-04-29 12:48:01 -0700

    App Compiles

---
+++ instagram-app/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+import './dummy-data.js';
+
+
+export default class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      userPosts : []
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+      
+      </div>
+    )
+  }
+}
+
+
